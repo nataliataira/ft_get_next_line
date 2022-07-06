022c2377379b92af1407d9aab83024144a83f04a
ngomes-t <ngomes-t@student.42sp.org.br>
2022-07-06 07:51:43 +0200

    version with memory leaks

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,106 @@
 /*   By: ngomes-t <ngomes-t@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 17:04:38 by ngomes-t          #+#    #+#             */
-/*   Updated: 2022/06/29 23:20:12 by ngomes-t         ###   ########.fr       */
+/*   Updated: 2022/07/06 04:39:50 by ngomes-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_strchr(char *str, int c)
+{
+	if (!str)
+		return (NULL);
+	while (*str)
+	{
+		if (*str == c)
+			return ((char *)str);
+		str++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*join;
+	int		len;
+	int		i;
+	int		j;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	join = (char *)malloc(sizeof(char) * (len + 1));
+	if (!join)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		join[i++] = s2[j++];
+	join[i] = '\0';
+	return (join);
+}
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	dup = (char *)malloc(sizeof (char) * (i + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_get_line(char *constant)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!constant[i])
+		return (NULL);
+	while (constant[i] && constant[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (constant[i] && constant[i] != '\n')
+	{
+		line[i] = constant[i];
+		i++;
+	}
+	if (constant[i] == '\n')
+	{
+		line[i] = constant[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
